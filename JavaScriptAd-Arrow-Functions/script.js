fb3c0404d77204ee38180f1a97cb89b0b44b9f24
Boogaerdtth <70010486+Boogaerdtth@users.noreply.github.com>
2020-09-30 08:53:38 +0200

    Add files via upload

---
+++ JavaScriptAd-Arrow-Functions/script.js
@@ -0,0 +1,43 @@
+// zet de functie om naar een arrow functie
+// const ikRockArrowFunctions = function () {
+//     console.log("Joe, ik rock de arrow functions!");
+// };
+
+// ikRockArrowFunctions()
+
+// opdracht A
+// const ikRockArrowFunctions = () => {
+//     console.log("Joe, ik rock de arrow functions!");
+// };
+
+// ikRockArrowFunctions()
+
+// const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+// ikRockArrowFunctions()
+
+
+
+// // opdracht B
+// const fivePlusSeven = function () {
+//     return 5 + 7
+// };
+
+// fivePlusSeven();
+
+// const fivePlusSeven = () => 5 + 7;
+
+// console.log(fivePlusSeven());
+
+
+// opdracht C
+// const myFunction = (a, b) => a + b;
+// console.log(myFunction(1, 2));
+
+// // Opdracht D
+// const addFive = a => a + 5;
+// console.log(addFive(2));
+
+// Opdracht E
+const createObject = () => ({ value: 'hoi' });
+console.log(createObject());
